11314989be521717b033f04f675d0aefb36dbd3a
Nick Yu <rednaxela64@outlook.com>
2022-09-08 13:19:03 -0700

    add views for each anecdote

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import { Routes, 
   Link, 
-  Route } from 'react-router-dom'
+  Route,
+useMatch } from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -20,8 +21,19 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+      <li key={anecdote.id} >
+        <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+      </li>)}
     </ul>
+  </div>
+)
+
+const Anecdote = ({ anecdote }) => (
+  <div>
+    <h2>{anecdote.content}</h2>
+    <p>has {anecdote.votes} votes</p>
+    <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
   </div>
 )
 
@@ -105,14 +117,19 @@ const App = () => {
   ])
 
   const [notification, setNotification] = useState('')
-
+  const match = useMatch('/anecdotes/:id')
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000)
     setAnecdotes(anecdotes.concat(anecdote))
   }
 
   const anecdoteById = (id) =>
-    anecdotes.find(a => a.id === id)
+    anecdotes.find(a => a.id === Number(id))
+
+  const matchedAnecdote = match 
+  ? anecdoteById(match.params.id)
+  : null
+  
 
   const vote = (id) => {
     const anecdote = anecdoteById(id)
@@ -125,6 +142,8 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+
+
   return (
     <div>
       <h1>Software anecdotes</h1>
@@ -132,6 +151,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
         <Route path='/anecdotes' element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path='/anecdotes/:id' element={<Anecdote anecdote={matchedAnecdote} />} />
         <Route path='/about' element={<About />} />
         <Route path='/create' element={<CreateNew addNew={addNew} />} />
       </Routes>
